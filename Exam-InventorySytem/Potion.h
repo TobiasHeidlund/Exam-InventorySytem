d036a369f5a4a6f77d1f5bfceb39e4b12bd7baa9
unknown <Carljohank@hotmail.com>
2024-09-16 11:47:33 +0200

    potion constructor

---
+++ Exam-InventorySytem/Potion.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include "Item.h"
+
+class Potion : Item{
+	public:
+		int healingAmount;
+		void use() override;
+		Potion(std::string name, int weight, int hA);
+		Potion();
+};
