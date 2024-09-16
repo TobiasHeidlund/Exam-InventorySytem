ea7dcf9aac7645f70f48c641bd3066ee51874614
Tobias <tobias199@gmail.com>
2024-09-16 12:41:19 +0200

    2 attempt of fixing duplication

--- Exam-InventorySytem/POTION.CPP
+++ Exam-InventorySytem/POTION.CPP
@@ -1,16 +1,27 @@
 #include "Potion.h"
 
-Potion::Potion(std::string name, int weight, int hA) : healingAmount(hA){
+Potion::Potion(std::string name, int weight, int hA) : healingAmount(hA) {
 	setName(name);
 	setWeight(weight);
 }
+Potion::Potion() {
+	cout << "Vad heter potion'en: ";
+	string name;
+	cin >> name;
+	setName(name);
 
-Potion::Potion()
-{
+	cout << "Vad väger potion'en: ";
+	int weight;
+	cin >> weight;
+	setWeight(weight);
+
+	cout << "hur mycket healar potion'en: ";
+	int heal;
+	cin >> heal;
+	this->healingAmount = heal;
 }
 
-void Potion::use() 
+void Potion::use()
 {
 	std::cout << "drinking " << Potion::name << " for " << Potion::healingAmount;
 }
-
