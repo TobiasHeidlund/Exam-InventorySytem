72f4a854743f36f0a728e2f225597da6ef8eee89
Tobias <tobias199@gmail.com>
2024-09-16 09:31:05 +0200

    added item

---
+++ Exam-InventorySytem/Item.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+class Item
+{
+	private:
+		std::string name;
+		int weight;
+	public:
+		virtual void use() = 0;
+		virtual ~Item();
+		std::string getName();
+		int getWeight();
+		void setName(std::string);
+		void setWeight(int);
+
+};
+
